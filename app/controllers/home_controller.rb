d203158089e1670ce2a3c3cd460666963f0d7a2e
Dimitris Klisiaris <dklisiaris@gmail.com>
2015-01-03 21:09:15 +0200

    Build a very simple search form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index 
 
-  def index
+  def index    
+    @search_results = Book.search(params[:search]).page(params[:page])
     @latest_books = Book.order(created_at: :desc).limit(5)   
   
   end
