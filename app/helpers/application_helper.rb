66fadb0f3300cc48da4861a8bc2d7cc0ccc7306d
Dimitris Klisiaris <dklisiaris@gmail.com>
2015-01-10 22:14:26 +0200

    Change navigation to corresponding urls. Apply theme to kaminari.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+def side_nav_li(text, path, icon=nil)
+  content_tag(:li) do   
+    options = current_page?(path) ? { class: "active" } : {}
+    if icon
+      link_to path, options do                           
+        "<i class='#{icon} sidebar-nav-icon'></i><span class='sidebar-nav-mini-hide'>#{text}</span>".html_safe
+      end 
+    else
+      link_to(text, path, options)
+    end
+  end
+end
+
+def top_nav_li(text, path, icon=nil)
+  content_tag(:li) do 
+    link_to path do                           
+      "<i class='#{icon}'></i><strong>#{text.upcase}</strong>".html_safe
+    end     
+  end
+end
+
 end
