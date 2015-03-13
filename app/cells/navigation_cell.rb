84f07ec351682dbf90c60bd9a4d9e959d5350712
Dimitris Klisiaris <dklisiaris@gmail.com>
2015-03-13 22:03:41 +0200

    Apply content header cell in books.

--- app/cells/navigation_cell.rb
+++ app/cells/navigation_cell.rb
@@ -1,5 +1,16 @@
 class NavigationCell < Cell::ViewModel  
 
+  # Produces a content header just below top navigation bar 
+  #
+  # ==== Options
+  #
+  # * +:title+ - The title of header
+  # * +:breadcrumbs+ - A hash of name-url values for each page in breadcrumbs. Last page is the current and must have a nil value.
+  #
+  # ==== Examples
+  #   
+  #   cell(:navigation).call(:content_header, title: 'Some title', breadcrumbs: { fa('home') => root_url, t('site_settings') => "#", 'Current Page' => nil })
+  #
   def content_header(opts = {})      
     @title        = opts[:title]  
     @breadcrumbs  = opts[:breadcrumbs]      
