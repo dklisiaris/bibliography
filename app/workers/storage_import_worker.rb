f3647e05f2a13e2c439b2925188f8753b9ec8585
Dimitris Klisiaris <dklisiaris@gmail.com>
2015-04-21 22:50:27 +0300

    Change some es settings.

--- app/workers/storage_import_worker.rb
+++ app/workers/storage_import_worker.rb
@@ -8,7 +8,7 @@ class StorageImportWorker
     #   ImportWorker.perform_async(data_hash)
     # end
     files_to_import = []
-    for i in 51..80 do
+    for i in 81..100 do
       files_to_import << Dir.glob("#{Rails.root}/tmp/storage/json_book_pages/#{i.to_s}/*.json")
     end
     files_to_import.flatten!
--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,6 +3,8 @@
 # Increase timeout.
 Searchkick.timeout = 60
 
+Searchkick.client = Elasticsearch::Client.new(url: ENV["ELASTICSEARCH_URL"], retry_on_failure: 5)
+
 # Use typhoeus for http requests instead of the default Net::HTTP
 require "typhoeus/adapters/faraday"
 Ethon.logger = Logger.new("/dev/null")
