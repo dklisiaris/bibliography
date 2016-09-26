f21983122c630252bf38158118acca49cc109c2b
Dimitris Klisiaris <dklisiaris@gmail.com>
2016-09-26 19:57:57 +0300

    Clean up unused files

--- vendor/assets/javascripts/pages/uiIcons.js
+++
@@ -1,5 +0,0 @@
-/*
- *  Document   : uiIcons.js
- *  Author     : pixelcave
- */
-var UiIcons=function(){return{init:function(){var t;$("#page-content .btn").click(function(){return t=$(this).attr("data-original-title"),$("#icon-gen-input").val('<i class="'+t+'"></i>').select(),$("html,body").animate({scrollTop:$("#icon-gen").offset().top-15}),!1})}}}();
