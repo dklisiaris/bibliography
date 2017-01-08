2aa5068e595fbdac773af7af6341ad0f4bcb90cc
Dimitris Klisiaris <dklisiaris@gmail.com>
2017-01-08 23:41:51 +0200

    Add threaded comments

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -49,7 +49,7 @@ class BooksController < ApplicationController
     @likes_count = @book.liked_by_count
     @dislikes_count = @book.disliked_by_count
 
-    @comments = @book.comment_threads.includes(:user)
+    @comments = @book.root_comments.includes(:children, :user, children: :user)
 
     impressionist(@book)
     respond_with(@book)
