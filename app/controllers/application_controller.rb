d1d7e3fdf5a109ddaf74dd6e5fbf9a1e8da001db
Dimitris Klisiaris <dimitrisk@eproductions.gr>
2017-04-26 18:08:13 +0300

    Add metatags in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   # Verify that controller actions are authorized. Optional, but good.
   after_action :verify_authorized,  except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
+  before_action :prepare_meta_tags, if: "request.get?"
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -23,6 +24,42 @@ class ApplicationController < ActionController::Base
 
   def set_json_format
     request.format = :json
+  end
+
+  def prepare_meta_tags(options={})
+    site_name   = "Bibliography GR - Greek books database, catalogs, recommendations"
+    title       = [controller_name, action_name].join(" ")
+    description = "Bibliography GR is database of hellenic books with social features"
+    image       = options[:image] || "default-image-url"
+    current_url = request.url
+
+    # Let's prepare a nice set of defaults
+    defaults = {
+      site:        site_name,
+      title:       title,
+      image:       image,
+      description: description,
+      keywords:    %w[books catalog recommendations reviews],
+      twitter: {
+        site_name: site_name,
+        site: '@bibliographygr',
+        card: 'summary',
+        description: description,
+        image: image
+      },
+      og: {
+        url: current_url,
+        site_name: site_name,
+        title: title,
+        image: image,
+        description: description,
+        type: 'website'
+      }
+    }
+
+    options.reverse_merge!(defaults)
+
+    set_meta_tags options
   end
 
   private
