8d44aaa05b513c514c53d4a618125ded87fb21c8
Dimitris Klisiaris <dklisiaris@gmail.com>
2017-08-26 17:11:34 +0300

    Use production as deploy environment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,8 +20,9 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
-
+role :app, [ENV["BIBLIOGRAPHY_CAP_SERVER"]]
+role :web, [ENV["BIBLIOGRAPHY_CAP_SERVER"]]
+role :db,  [ENV["BIBLIOGRAPHY_CAP_SERVER"]]
 
 # Configuration
 # =============
