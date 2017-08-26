8cb73f3cc532305085e22a56819ac5f05a99087d
Dimitris Klisiaris <dklisiaris@gmail.com>
2017-08-26 20:57:41 +0300

    Change in elastic settings

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -5,6 +5,8 @@ Searchkick.timeout = 60
 
 # Searchkick.client = Elasticsearch::Client.new(url: ENV["ELASTICSEARCH_URL"], retry_on_failure: 5)
 
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || "localhost:9200"
+
 Searchkick.client_options = {
   retry_on_failure: true
 }
