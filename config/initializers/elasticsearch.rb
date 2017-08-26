709a68345a1e13c2f2b488ae4ff075d0b83c1793
Dimitris Klisiaris <dklisiaris@gmail.com>
2017-08-26 20:52:12 +0300

    Change some elasticsearch settings

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,8 +3,12 @@
 # Increase timeout.
 Searchkick.timeout = 60
 
-Searchkick.client = Elasticsearch::Client.new(url: ENV["ELASTICSEARCH_URL"], retry_on_failure: 5)
+# Searchkick.client = Elasticsearch::Client.new(url: ENV["ELASTICSEARCH_URL"], retry_on_failure: 5)
+
+Searchkick.client_options = {
+  retry_on_failure: true
+}
 
 # Use typhoeus for http requests instead of the default Net::HTTP
-require "typhoeus/adapters/faraday"
+# require "typhoeus/adapters/faraday"
 Ethon.logger = Logger.new("/dev/null")
