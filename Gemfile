8c4d77411e50aff05db3723d702edc5ab0c0edef
Dimitris Klisiaris <dklisiaris@gmail.com>
2017-08-27 23:15:00 +0300

    Use default es settings

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'turbolinks', '~> 2.5'
 gem 'jbuilder', '~> 2.2'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-gem 'thor', '0.19.1'
+gem 'thor', '~> 0.20.0'
 
 # High Performance Haml Implementation
 gem 'hamlit-rails', '~> 0.1.0'
@@ -132,7 +132,7 @@ gem 'searchkick', '~> 2.3'
 gem 'jquery-turbolinks', '~> 2.1'
 
 # Runs HTTP requests in parallel while cleanly encapsulating handling logic.
-gem 'typhoeus', '~> 1.0'
+gem 'typhoeus', '~> 1.3'
 
 # Repository for collecting Locale data for Ruby on Rails I18n as well as other interesting, Rails related I18n stuff
 gem 'rails-i18n', '~> 4.0.0' # For 4.0.x
--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,18 +1,18 @@
 # Elasticsearch configuration.
 
 # Use typhoeus for http requests instead of the default Net::HTTP
-require "typhoeus/adapters/faraday"
-Ethon.logger = Logger.new("/dev/null")
+# require "typhoeus/adapters/faraday"
+# Ethon.logger = Logger.new("/dev/null")
 
 # Increase timeout.
-Searchkick.timeout = 60
+# Searchkick.timeout = 60
 
 # Searchkick.client = Elasticsearch::Client.new(url: ENV["ELASTICSEARCH_URL"], retry_on_failure: 5)
 
-Searchkick.client_options = {
-  retry_on_failure: true,
-  host: ENV["ELASTICSEARCH_URL"],
-  adapter: :typhoeus
-}
+# Searchkick.client_options = {
+#   retry_on_failure: true,
+#   host: ENV["ELASTICSEARCH_URL"],
+#   adapter: :typhoeus
+# }
 
 
