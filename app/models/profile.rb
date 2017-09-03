50f20aebb76fe77ed85f05e8752bd479b37c89fc
Dimitris Klisiaris <dklisiaris@gmail.com>
2017-09-04 00:00:59 +0300

    Use https for showing gravatars

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -26,7 +26,7 @@ class Profile < ActiveRecord::Base
       avatar_url
     else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "http://gravatar.com/avatar/#{gravatar_id}.png"
+      "https://gravatar.com/avatar/#{gravatar_id}.png"
     end
   end
 
