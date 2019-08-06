1a06773d1cb10e2b1f6b8aa9dae28e6cdd0c360a
Dimitris N Klisiaris <dnk@empist.com>
2019-08-06 15:07:13 +0300

    Add all links to sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,37 +1,14 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://bibliography.gr"
+SitemapGenerator::Sitemap.default_host = 'https://bibliography.gr'
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
-
   add categories_path
-  Category.where("impressions_count > ?", 0).each do |record|
+  Category.find_each do |record|
     add category_path(record.slug), lastmod: record.updated_at
   end
 
   add authors_path
-  Author.where("impressions_count > ?", 0).each do |record|
+  Author.find_each do |record|
     add author_path(record.slug), lastmod: record.updated_at
   end
 
@@ -40,17 +17,17 @@ SitemapGenerator::Sitemap.create do
   add awarded_books_path
   add latest_books_path
   add featured_books_path
-  Book.where("impressions_count > ?", 0).each do |record|
+  Book.find_each do |record|
     add book_path(record.slug), lastmod: record.updated_at
   end
 
   add publishers_path
-  Publisher.where("impressions_count > ?", 0).each do |record|
+  Publisher.find_each do |record|
     add publisher_path(record.slug), lastmod: record.updated_at
   end
 
   add series_index_path
-  # Series.find_each do |record|
-  #   add books_path(series: record.name), lastmod: record.updated_at
-  # end
+  Series.find_each do |record|
+    add books_path(series: record.name), lastmod: record.updated_at
+  end
 end
