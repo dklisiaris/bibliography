989cb9809298b9b9268709cc2b9497da02956d6e
Dimitris Klisiaris <dklisiaris@gmail.com>
2021-03-17 22:50:04 +0200

    Fix issue with shelf policy

--- app/policies/shelf_policy.rb
+++ app/policies/shelf_policy.rb
@@ -1,12 +1,12 @@
 class ShelfPolicy < ApplicationPolicy
 
-  def index?  ; belongs_to_current_user? ; end
-  def show?   ; belongs_to_current_user? ; end
-  def create? ; belongs_to_current_user? ; end
-  def new?    ; belongs_to_current_user? ; end
-  def update? ; belongs_to_current_user? ; end
-  def edit?   ; belongs_to_current_user? ; end
-  def destroy?; belongs_to_current_user? and not record.built_in ; end
+  def index?  ; shelf_belongs_to_current_user? ; end
+  def show?   ; shelf_belongs_to_current_user? ; end
+  def create? ; shelf_belongs_to_current_user? ; end
+  def new?    ; shelf_belongs_to_current_user? ; end
+  def update? ; shelf_belongs_to_current_user? ; end
+  def edit?   ; shelf_belongs_to_current_user? ; end
+  def destroy?; shelf_belongs_to_current_user? and not record.built_in ; end
 
   def public_shelves?
     if record.is_a? Symbol
@@ -15,10 +15,17 @@ class ShelfPolicy < ApplicationPolicy
       (record.is_public? || (record.user.profile.is_public? && record.same_as_profile?))
     end
   end
-
   class Scope < Scope
     def resolve
       scope.where(user_id: user.id)
     end
+  end
+
+  protected
+
+  def shelf_belongs_to_current_user?
+    return false if user.blank?
+
+    user && (record.user == user) || record.user.profile.privacy == 'is_public'
   end
 end
