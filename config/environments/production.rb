054e4857eb94bacd1ca75a0a51afae30c25de4bd
Dimitris Klisiaris <dklisiaris@gmail.com>
2026-06-14 22:26:07 +0300

    use harmony in uglifier js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # Rails 7 @rails/ujs uses ES6 (const); Uglifier needs harmony mode.
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :scss
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
