1de9ec8b5e41bc1bf6c1c0ea5129b92dc62d3c1c
Dimitris Klisiaris <dklisiaris@gmail.com>
2026-06-25 00:11:22 +0300

    Clean up legacy files (#98)
    * remove glyphicons
    
    * remove animation.css
    
    * remove jquery ready selector

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,8 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
+# Sprockets serves images/fonts via manifest.js; JS/CSS come from jsbundling/cssbundling builds.
 Rails.application.config.assets.precompile += %w[hotwire.js hotwire.css]
 
 # hotwire.css is built by Dart Sass (npm run build:css) and contains modern CSS
