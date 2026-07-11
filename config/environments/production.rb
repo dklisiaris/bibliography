23b0bd7e6ef14af93e8fcbaed98ea4f369211f8f
Dimitris Klisiaris <dklisiaris@gmail.com>
2026-07-11 11:40:23 +0300

    replace sprockets with propshaft (#109)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,19 +27,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Terser supports modern ES (esbuild hotwire.js / Turbo); Uglifier does not.
-  config.assets.js_compressor = :terser
-  # config.assets.css_compressor = :scss
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true # Should be false in production, but assets arent loading. Needs further investigation.
-  # config.assets.precompile =  ['*.js', '^[^_]*.css', '*.css.erb']
-
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
-  config.assets.digest = true
-
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  # Do not fallback to runtime asset compilation if a precompiled asset is missed.
+  config.assets.compile = false
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
